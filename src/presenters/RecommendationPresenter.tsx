92bdd35b06241eeff4d9bd712380ca7fcba1b1e7
Jonathan Erikson <jerikso@kth.se>
2023-12-08 12:13:10 +0100

    Fetch recommendations based on selected top artists and tracks

--- src/presenters/RecommendationPresenter.tsx
+++ src/presenters/RecommendationPresenter.tsx
@@ -3,36 +3,85 @@ import RecommendationsView from '../views/RecommendationView';
 import {Model} from '../interfaces';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { fetchRecommendations } from '../spotifyFetcher';
+import { fetchRecommendations, fetchTopItems } from '../spotifyFetcher';
 import { set } from 'mobx';
-
-interface Props {
-    model: Model,
-}
 
 
 export default
 observer (
-        function Recommendations(props: Props) {
+        function Recommendations(props: {model: any}) {
             // this assumes that a UserModel is given...
             const navigate = useNavigate();
-            console.log("props.model", props.model);
-            if (!props.model.userAuthToken) {
-                // navigate to login
-                // TODO: add notification that you need to login
-                return <></>
+        if (!props.model.accessToken) {
+            // navigate to login
+            // TODO: add notification that you need to login
+            navigate("/");
+        }
+
+        const accessToken = props.model.accessToken || "";
+        const [items, setItems] = useState([]);
+        const [seedArtists, setSeedArtists] = useState<string[]>([]);
+        const [seedTracks, setSeedTracks] = useState<string[]>([]);
+        const [topArtists, setTopArtists] = useState<string[]>([]);
+        const [topTracks, setTopTracks] = useState<string[]>([]);
+        //const [timeRange, setTimeRange] = useState<string>("medium_term");
+
+        function onArtistSelectedACB(id: string, name:string) {
+            console.log("artist selected", name);
+            if (seedArtists.includes(id)) {
+                setSeedArtists(seedArtists.filter((id) => id != id));
+            } else {
+                setSeedArtists(seedArtists.concat(id));
             }
+            console.log("seed artists", seedArtists);
+        }
+        
+        function onTrackSelectedACB(id: string, name:string) {
+            console.log("track selected", name);
+            if (seedTracks.includes(id)) {
+                setSeedTracks(seedTracks.filter((id) => id != id));
+            } else {
+                setSeedTracks(seedTracks.concat(id));
+            }
+            console.log("seed tracks", seedTracks);
+        }
+        
+        const numRecommendations = 20;
+        const numTopItems = 20;
 
-            const accessToken = props.model.userAuthToken || "";
-            const [items, setItems] = useState([]);
+        function getRecommendationsACB() {
+            const seedGenres = seedArtists.length == 0  && seedTracks.length == 0 ?  ["pop"] : [];
+            fetchRecommendations(accessToken, numRecommendations, seedArtists, seedTracks, seedGenres)
+            .then((items) => { 
+                setItems(items); 
+            })
+        }
 
-            // Fetch items on load
-            useEffect(() => { fetchRecommendations(accessToken, 20, [], [], ["pop"])
-                .then((items) => { 
-                    setItems(items); 
-                })
-            }, []);
+        function getTopArtistsACB() {
+            fetchTopItems(accessToken, "artists", numTopItems)
+            .then((items) => { 
+                setTopArtists(items); 
+            })
+        }
 
-        return <RecommendationsView recommendations={items}/>;
+        function getTopTracksACB() {
+            fetchTopItems(accessToken, "tracks", numTopItems)
+            .then((items) => { 
+                setTopTracks(items); 
+            })
+        }
+
+        // get seed artists
+        // Fetch items on load
+        function onLoadACB() {
+            getTopArtistsACB();
+            getTopTracksACB();
+            getRecommendationsACB();
+        }
+
+        useEffect(onLoadACB, []);
+        useEffect(getRecommendationsACB, [seedArtists, seedTracks]); // update recommendations when seed artists or tracks change
+
+        return <RecommendationsView topArtists={topArtists} topTracks={topTracks} recommendations={items} onArtistSelected={onArtistSelectedACB} onTrackSelected={onTrackSelectedACB}/>;
     }
 );
--- src/views/RecommendationView.tsx
+++ src/views/RecommendationView.tsx
@@ -1,12 +1,42 @@
-function RecommendationsView(props: {recommendations: string[];}) {
+function RecommendationsView(props: {recommendations: string[]; topArtists: string[]; topTracks: string[]; onArtistSelected: (id: string, name: string) => void; onTrackSelected: (id: string, name: string) => void}) {
     function recommendationACB(recommendation: any) {
         return <li>{recommendation.name}</li>;
     }
-    return (
-        <ul>
-            {props.recommendations?.map(recommendationACB)}
-        </ul>        
+    function trackACB(track: any) {
+        return (
+            <div>
+                <input type="checkbox" id={track.id} name={track.id} onChange={() => {props.onArtistSelected(track.id, track.name)}}/>
+                <label htmlFor={track.id}>{track.name}</label>
+            </div>
         );
+    }
+
+    function artistACB(artist: any) {
+        return (
+            <div>
+                <input type="checkbox" id={artist.id} name={artist.id} onChange={() => {props.onArtistSelected(artist.id, artist.name)}}/>
+                <label htmlFor={artist.id}>{artist.name}</label>
+            </div>
+        );
+    }
+
+    return (
+        <>
+            <fieldset>
+                <legend>Choose artists:</legend>
+                {props.topArtists?.map(trackACB)}
+            </fieldset>
+
+            <fieldset>
+                <legend>Choose tracks:</legend>
+                {props.topTracks?.map(trackACB)}
+            </fieldset>
+
+            <ul>
+                {props.recommendations?.map(recommendationACB)}
+            </ul>   
+        </>    
+    );
 }
 
 export default RecommendationsView;
