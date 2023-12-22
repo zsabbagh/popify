2e58d3816f949365924b05f993a3d0e9aa3fb44f
William Redgård <william.redgard@gmail.com>
2023-12-22 15:08:37 +0100

    playlist history added to userpage

--- src/views/UserView.tsx
+++ src/views/UserView.tsx
@@ -1,27 +1,47 @@
-import { Avatar, Paper } from "@mui/material";
-import { SpotifyTrack, User } from "../interfaces";
+import { Avatar, Paper } from '@mui/material';
+import { SpotifyTrack, User } from '../interfaces';
 
-interface Props{
+interface Props {
   playlists: SpotifyTrack[][];
-  user: User
+  user: User;
 }
 
-export default
-function UserView(props: Props) {
+export default function UserView(props: Props) {
   const user = props.user;
-  console.log("playlist", props.playlists);
-  
+  const playlists = props.playlists;
+
   return (
-    <div style={{textAlign: "center"}}>
-      <div style={{width: "100%", marginTop: "20px"}}>
-        <img style={{borderRadius: "50%", width: "250px"}} src={user.images[user.images.length - 1].url}></img>
+    <div style={{ textAlign: 'center' }}>
+      <div style={{ width: '100%', marginTop: '20px' }}>
+        <img style={{ borderRadius: '50%', width: '250px' }} src={user.images[user.images.length - 1].url}></img>
       </div>
       <h1>{user.display_name}</h1>
 
-      <Paper style={{display: "flex", flexDirection: "row"}} elevation={2}>
-        
-
+      <Paper
+        style={{ margin: '20px', display: 'flex', flexDirection: 'row', padding: '20px', gap: '20px' }}
+        elevation={2}
+      >
+        {playlists.map((playlist) => (
+          <Paper elevation={3} style={{ width: 200, height: 200 }}>
+            <img
+              style={{ width: 100, height: 100 }}
+              src={playlist[0].album.images[playlist[0].album.images.length - 1].url}
+            ></img>
+            <img
+              style={{ width: 100, height: 100 }}
+              src={playlist[1].album.images[playlist[0].album.images.length - 1].url}
+            ></img>
+            <img
+              style={{ width: 100, height: 100 }}
+              src={playlist[2].album.images[playlist[0].album.images.length - 1].url}
+            ></img>
+            <img
+              style={{ width: 100, height: 100 }}
+              src={playlist[3].album.images[playlist[0].album.images.length - 1].url}
+            ></img>
+          </Paper>
+        ))}
       </Paper>
     </div>
   );
-};
+}
